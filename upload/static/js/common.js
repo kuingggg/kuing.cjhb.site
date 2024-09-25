dbbc715162ad9f765c6fd5c078f7593e6a6d23d5
kuingggg <249533164@qq.com>
2024-09-25 16:14:04 +0000

    update upload/static/js/common.js. 粘贴自论坛上的
    Signed-off-by: kuingggg <249533164@qq.com>

--- upload/static/js/common.js
+++ upload/static/js/common.js
@@ -1895,7 +1895,7 @@ function noticeTitle() {
 
 function noticeTitleFlash() {
 	if(NOTICETITLE.flashNumber < 5 || NOTICETITLE.flashNumber > 4 && !NOTICETITLE['State']) {
-		document.title = (NOTICETITLE['State'] ? '【　　　】' : '【新提醒】') + NOTICETITLE['oldTitle'];
+		//document.title = (NOTICETITLE['State'] ? '【　　　】' : '【新提醒】') + NOTICETITLE['oldTitle'];//kk del
 		NOTICETITLE['State'] = !NOTICETITLE['State'];
 	}
 	NOTICETITLE.flashNumber = NOTICETITLE.flashNumber < NOTICETITLE.sleep ? ++NOTICETITLE.flashNumber : 0;
