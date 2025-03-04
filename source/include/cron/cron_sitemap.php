e769cdf50bac0bad1069af2c7db5d67c822d8c62
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-03-04 22:53:41 +0000

    Discuz!运行日志记录函数runlog详解
    https://blog.csdn.net/lih062624/article/details/68943456

--- source/include/cron/cron_sitemap.php
+++ source/include/cron/cron_sitemap.php
@@ -1,39 +1,51 @@
 <?php
-if(!defined('IN_DISCUZ')) {
-exit('Access Denied');
+if (!defined('IN_DISCUZ')) {
+    exit('Access Denied');
 }
 
-$filename='sitemap.xml';
-//以下五项根据具体情况修改即可
-$cfg_updateperi='60';//协议文件更新周期的上限，单位为分钟
-$web_root=$_G['siteurl'];//根网址
-$CHARSET='utf-8';// or gbk //选择编码方式
+$filename = 'sitemap.xml';
+// 以下五项根据具体情况修改即可
+$cfg_updateperi = '60'; // 协议文件更新周期的上限，单位为分钟
+$web_root = $_G['siteurl']; // 根网址
+$CHARSET = 'utf-8'; // or gbk // 选择编码方式
+
 /***********************************************************************************************/
-//网站地图sitemap.xml
-$sitemap="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-$sitemap.="<urlset\n";
-$sitemap.="xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"\n";
-$sitemap.="xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n";
-$sitemap.="xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9\n";
-$sitemap.="http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n";
-$querys = DB::query("SELECT a.tid FROM ".DB::table('forum_thread')." a inner join ".DB::table('forum_forum')." b on a.fid=b.fid ORDER BY a.tid DESC LIMIT 0,10000");
-while($threadfid = DB::fetch($querys))
-{
-$turl=$web_root.'thread-'.$threadfid['tid'].'-1-1.html';//注意静态规则
-$link = $turl;
-$t=time();
-$riqi=date("Y-m-d",$t);
-$priority=rand(1,10)/10;
-//date("D F d Y",$t);
-$sitemap.="<url>\n";
-$sitemap.="<loc>$link</loc>\n";
-$sitemap.="<priority>$priority</priority>\n";
-$sitemap.="<lastmod>$riqi</lastmod>\n";
-$sitemap.="<changefreq>weekly</changefreq>\n";
-$sitemap.="</url>\n";
+// 网站地图sitemap.xml
+$sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+$sitemap .= "<urlset\n";
+$sitemap .= "xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"\n";
+$sitemap .= "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n";
+$sitemap .= "xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9\n";
+$sitemap .= "http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n";
+
+$querys = DB::query("SELECT a.tid FROM " . DB::table('forum_thread') . " a INNER JOIN " . DB::table('forum_forum') . " b ON a.fid = b.fid ORDER BY a.tid DESC LIMIT 0,10000"); // 限制10000条
+
+if ($querys) {
+    while ($threadfid = DB::fetch($querys)) {
+        $turl = $web_root . 'thread-' . $threadfid['tid'] . '-1-1.html'; // 注意静态规则
+        $link = htmlspecialchars($turl, ENT_QUOTES, 'UTF-8'); // 防止特殊字符
+        $t = time(); // 当前时间
+        $riqi = date("Y-m-d", $t); // 日期
+        $priority = rand(1, 10) / 10; // 优先级
+
+        $sitemap .= "<url>\n";
+        $sitemap .= "<loc>$link</loc>\n";
+        $sitemap .= "<priority>$priority</priority>\n";
+        $sitemap .= "<lastmod>$riqi</lastmod>\n";
+        $sitemap .= "<changefreq>weekly</changefreq>\n";
+        $sitemap .= "</url>\n";
+    }
+} else {
+    runlog('error',"Database query failed: " . DB::error()); // 记录错误日志
 }
+
 $sitemap .= "</urlset>\n";
-$fp = fopen(DISCUZ_ROOT.'/'.$filename,'w');
-fwrite($fp,$sitemap);
-fclose($fp);
+
+$filepath = DISCUZ_ROOT . '/' . $filename;
+if ($fp = fopen($filepath, 'w')) {
+    fwrite($fp, $sitemap);
+    fclose($fp);
+} else {
+    runlog('error',"Failed to open file for writing: $filepath"); // 记录错误日志
+}
 ?>
